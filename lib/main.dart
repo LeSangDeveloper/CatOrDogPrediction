2466f1a1f4d9c3bd7119a4c1b573af2bdb68dd04
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-11-22 13:23:00 +0800

    init flutter app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Cat or Dog",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Cat Or Dog")
+        ),
+        body: Center(
+          child: const Text("Dog or Cat"),
+        ),
+      ),
+    );
+  }
+}
+
