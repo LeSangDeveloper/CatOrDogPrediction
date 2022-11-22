beead25759501ca10a103c2af4cb8cb2ef97c20a
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-11-22 13:51:44 +0800

    init imageview and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,77 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Cat or Dog",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Cat Or Dog")
-        ),
-        body: const Center(
-          child: Text("Dog or Cat"),
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late File _imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Cat Dog Identifier")
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                margin: const EdgeInsets.all(15),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  border: Border.all(color: Colors.white),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(2, 2),
+                      spreadRadius: 2,
+                      blurRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Image.network('https://i.imgur.com/sUFH1Aq.png')
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  debugPrint('Load Image test');
+                },
+                child: const Icon(Icons.camera)
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
 
